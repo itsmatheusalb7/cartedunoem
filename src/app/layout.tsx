a539bf3d0702ac15eca4e12c8d0e6fa5945dd5cc
Julia Silva <juliasilva9032@gmail.com>
2025-10-15 20:48:22 +0000

    COLOQUE ESSE CÓDIGO NO HEAD DO SITE, É O PIXEL DO FACEBOOK
    <!-- Meta Pi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,29 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '733558265799286');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
       <body className="font-body antialiased">
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=733558265799286&ev=PageView&noscript=1"
+          />
+        </noscript>
         {children}
         <Toaster />
       </body>
